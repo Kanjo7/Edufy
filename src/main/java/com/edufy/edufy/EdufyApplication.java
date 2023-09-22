a832969d2a01354e10f838b9590e2b4ea5d619f8
Kanjo7 <alex_kanjo@hotmail.com>
2023-09-22 14:28:34 +0200

    1.0

---
+++ src/main/java/com/edufy/edufy/EdufyApplication.java
@@ -0,0 +1,13 @@
+package com.edufy.edufy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdufyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdufyApplication.class, args);
+    }
+
+}
