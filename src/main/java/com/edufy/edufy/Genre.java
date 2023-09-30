afe048746258b76715c397d81411552ca0dbeb33
Kanjo7 <alex_kanjo@hotmail.com>
2023-09-30 20:59:08 +0200

    1.2

--- src/main/java/com/edufy/edufy/Genre.java
+++ src/main/java/com/edufy/edufy/Genre.java
@@ -1,10 +1,7 @@
 package com.edufy.edufy;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Genre {
@@ -13,6 +10,7 @@ public class Genre {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Column(length = 20)
     String genre;
 
     public Genre() {
