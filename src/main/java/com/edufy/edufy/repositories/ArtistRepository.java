db22b7d1c97e32cb3d05789320e24567d3f95122
mickepetersson <per.mikael.petersson@gmail.com>
2023-09-30 13:31:03 +0200

    set up h2 db + aÃpp properties

--- src/main/java/com/edufy/edufy/repositories/ArtistRepository.java
+++ src/main/java/com/edufy/edufy/repositories/ArtistRepository.java
@@ -1,4 +1,9 @@
 package com.edufy.edufy.repositories;
 
-public interface ArtistRepository {
+import com.edufy.edufy.models.Artist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArtistRepository extends JpaRepository<Artist, Integer> {
 }
