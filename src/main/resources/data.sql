dbbfe32c67bb8f81e85ab80f0c9b0cea7e0bc8b8
nicolinalarsson <nicolinalarsson1@gmail.com>
2023-10-10 11:06:48 +0200

    :)test

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO podcasts (title, duration, genre)
+
+VALUES ('En varg söker sin pod', '1h 6min', 'Samhälle och kultur'),
+       ('Studio Allsvenskan', '1h 12min', 'Sport'),
+       ('Filosofiska rummet', '59min', 'Nyheter'),
+       ('Förodmspodden', '59min', 'Underhållning');
